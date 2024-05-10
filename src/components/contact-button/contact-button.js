56c472688d37ae557f4e2154aafb5c44b7f97c28
NotBaldrick <hello@mikenew.xyz>
2024-05-10 12:49:47 +0100

    redo contact button component

--- src/components/contact-button/contact-button.js
+++ src/components/contact-button/contact-button.js
@@ -2,79 +2,35 @@
 import { Chats } from "@phosphor-icons/react/dist/ssr";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import Button from "../button/button";
 
 const MotionLink = motion(Link);
-const MotionChats = motion(Chats);
 
-const containerVariants = {
-	hidden: { opacity: 0, y: "100%", scale: 1 },
-	show: {
+const LinkVariant = {
+	inital: {
+		opacity: 0,
+		y: "100%",
+	},
+	animate: {
 		opacity: 1,
 		y: "0%",
-		transition: {
-			delayChildren: 0.5,
-			staggerChildren: 0.3,
-		},
-	},
-	hover: {
-		y: "-10%",
 	},
 };
 
-const textContainerVariants = {
-	show: {
-		transition: {
-			delayChildren: 0.1,
-			staggerChildren: 0.1,
-		},
-	},
-};
-
-const itemVariants = {
-	hidden: {
-		opacity: 0,
-		x: "-100%",
-		scale: 1,
-	},
-	show: {
-		opacity: 1,
-		x: "-0%",
-		scale: 1,
-	},
-};
-
-export default function ContactButton({ href, ...rest }) {
+export default function ContactButton({ href }) {
 	return (
 		<div className="pointer-events-none fixed left-0 top-0 z-50 flex h-full w-full items-end justify-end p-4">
 			<MotionLink
-				className="pointer-events-auto flex w-full items-center justify-center gap-2 border-2 p-1 sm:w-fit sm:p-2"
+				className="pointer-events-auto w-full sm:w-fit"
 				href={href}
-				variants={containerVariants}
-				initial="hidden"
-				animate="show"
-				whileHover="hover"
-				{...rest}
+				variants={LinkVariant}
+				initial="inital"
+				animate="animate"
 			>
-				<MotionChats
-					className="text-xl sm:text-3xl"
-					weight="regular"
-					variants={itemVariants}
-				/>
-
-				<motion.p
-					className="mb-0.5 text-lg uppercase sm:text-2xl"
-					variants={textContainerVariants}
-				>
-					{"contact".split("").map((l, i) => (
-						<motion.span
-							key={i}
-							className="inline-block"
-							variants={itemVariants}
-						>
-							{l}
-						</motion.span>
-					))}
-				</motion.p>
+				<Button className="flex w-full items-center justify-center gap-2 p-1 sm:p-2">
+					<Chats className="text-xl sm:text-3xl" weight="regular" />
+					<p className="mb-0.5 text-lg uppercase sm:text-2xl">contact</p>
+				</Button>
 			</MotionLink>
 		</div>
 	);
