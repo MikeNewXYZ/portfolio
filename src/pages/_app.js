65e67bb2c00d208e083a635ad370f6ddf3a0d90f
NotBaldrick <hello@mikenew.xyz>
2024-05-12 16:31:54 +0100

    add page transition

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	const router = useRouter();
+
+	return (
+		<AnimatePresence mode="wait" initial={false}>
+			<motion.div
+				key={router.pathname}
+				initial={{ opacity: 0, scale: 0.5 }}
+				animate={{ opacity: 1, scale: 1 }}
+				exit={{ opacity: 0, scale: 0.5 }}
+				transition={{ duration: 0.3, ease: "easeInOut" }}
+			>
+				<Component {...pageProps} />
+			</motion.div>
+		</AnimatePresence>
+	);
 }
