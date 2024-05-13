71dcd2846766e273f9219a6c6313907475b6b663
NotBaldrick <hello@mikenew.xyz>
2024-05-13 17:11:42 +0100

    add port

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,10 +1,12 @@
 import nodemailer from "nodemailer";
 
+const port = process.env.PORT || 3000;
 const email = process.env.EMAIL;
 const pass = process.env.EMAIL_PASS;
 
 export const transporter = nodemailer.createTransport({
 	service: "gmail",
+	port: port,
 	auth: {
 		user: email,
 		pass: pass,
