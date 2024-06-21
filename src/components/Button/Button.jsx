80b84f4a82de6c4d2f1f3a408e70a43917de5f6a
Mike Newberry <hello@mikenew.xyz>
2024-06-21 17:14:31 +0100

    change default button padding

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,10 +2,7 @@ import { twMerge } from "tailwind-merge";
 
 export default function Button({ className, children, ...props }) {
 	return (
-		<button
-			{...props}
-			className={twMerge("border-2 px-3 pb-1.5 pt-1 text-sm uppercase", className)}
-		>
+		<button {...props} className={twMerge("border-2 px-3 py-1.5 text-sm uppercase", className)}>
 			{children}
 		</button>
 	);
