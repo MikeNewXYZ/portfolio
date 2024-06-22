4e65a7171a701d4e1eb61caed53f11f6b38efbbc
Mike Newberry <hello@mikenew.xyz>
2024-06-22 13:08:46 +0100

    add button animation

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,13 @@ import { twMerge } from "tailwind-merge";
 
 export default function Button({ className, children, ...props }) {
 	return (
-		<button {...props} className={twMerge("border-2 px-3 py-1.5 text-sm uppercase", className)}>
+		<button
+			{...props}
+			className={twMerge(
+				"border-2 px-3 py-1.5 text-sm uppercase transition-transform duration-500 ease-in-out hover:-translate-y-1 active:scale-95",
+				className,
+			)}
+		>
 			{children}
 		</button>
 	);
