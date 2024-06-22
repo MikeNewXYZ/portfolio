123c2d242ada3d0d5f0e7aa4f4f740e735db4fb3
Mike Newberry <hello@mikenew.xyz>
2024-06-22 12:31:27 +0100

    add link to home hero buttons

--- src/components/HomeHero/HomeHero.jsx
+++ src/components/HomeHero/HomeHero.jsx
@@ -1,8 +1,10 @@
 import { useContext } from "react";
 import { HomeTitleContext } from "@context/AppContext";
 import Button from "@components/Button/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function HomeHero() {
+	const navigate = useNavigate();
 	const { homeTitle, setHomeTitle, resetHomeTitle } = useContext(HomeTitleContext);
 
 	return (
@@ -18,15 +20,21 @@ export default function HomeHero() {
 				>
 					<Button
 						className="px-2 py-1 sm:text-base"
+						onClick={() => navigate("/resume")}
 						onMouseEnter={() => setHomeTitle("resume")}
 						onMouseLeave={resetHomeTitle}
+						role="link"
+						aria-label="go to resume page"
 					>
 						resume
 					</Button>
 					<Button
 						className="px-2 py-1 sm:text-base"
+						onClick={() => navigate("/projects")}
 						onMouseEnter={() => setHomeTitle("projects")}
 						onMouseLeave={resetHomeTitle}
+						role="link"
+						aria-label="go to projects page"
 					>
 						projects
 					</Button>
