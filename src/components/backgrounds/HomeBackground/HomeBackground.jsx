3f5e8a251495683b4715978ece022b5fc01205cb
Mike Newberry <hello@mikenew.xyz>
2024-06-23 19:47:27 +0100

    add Home Background

---
+++ src/components/backgrounds/HomeBackground/HomeBackground.jsx
@@ -0,0 +1,27 @@
+import { ShootingStar } from "@phosphor-icons/react";
+import styles from "./styles.module.css";
+
+export default function HomeBackground() {
+	return (
+		<div className="fixed left-0 top-0 -z-10 h-dvh w-full" style={{ perspective: "1000px" }}>
+			<div className={`${styles.vignette} absolute left-0 top-0 z-20 h-full w-full`}></div>
+			<div
+				className={`${styles["star-wrapper"]} absolute left-0 top-0 z-10 h-full w-full text-xl antialiased`}
+			>
+				<ShootingStar
+					className={`${styles.star} absolute -translate-x-16 translate-y-16 rotate-180`}
+					weight="fill"
+				/>
+				<ShootingStar
+					className={`${styles.star} absolute -translate-x-4 -translate-y-20 rotate-180`}
+					weight="fill"
+				/>
+				<ShootingStar
+					className={`${styles.star} absolute translate-x-32 rotate-180`}
+					weight="fill"
+				/>
+			</div>
+			<div className={`${styles.background} h-full w-full`}></div>
+		</div>
+	);
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import HomeHero from "@/components/HomeHero/HomeHero";
+import HomeBackground from "@/components/backgrounds/HomeBackground/HomeBackground";
 import ContactButton from "@/components/ContactButton/ContactButton";
 
 export default function HomePage() {
@@ -6,6 +7,7 @@ export default function HomePage() {
 		<>
 			<HomeHero />
 			<ContactButton />
+			<HomeBackground />
 		</>
 	);
 }
