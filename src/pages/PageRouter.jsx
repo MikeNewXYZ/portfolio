525c3334258457a873ac5e2996cdf3b618701eff
Mike Newberry <hello@mikenew.xyz>
2024-06-25 13:42:57 +0100

    add page transition

--- src/pages/PageRouter.jsx
+++ src/pages/PageRouter.jsx
@@ -1,22 +1,28 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootContainer from "./RootContainer";
 import HomePage from "@pages/HomePage/HomePage";
 
 const router = createBrowserRouter([
 	{
-		path: "/",
-		element: <HomePage />,
-	},
-	{
-		path: "/contact",
-		element: <h1>contact page</h1>,
-	},
-	{
-		path: "/resume",
-		element: <h1>resume page</h1>,
-	},
-	{
-		path: "/projects",
-		element: <h1>projects page</h1>,
+		element: <RootContainer />,
+		children: [
+			{
+				index: true,
+				element: <HomePage />,
+			},
+			{
+				path: "/contact",
+				element: <h1>contact page</h1>,
+			},
+			{
+				path: "/resume",
+				element: <h1>resume page</h1>,
+			},
+			{
+				path: "/projects",
+				element: <h1>projects page</h1>,
+			},
+		],
 	},
 ]);
 
---
+++ src/pages/RootContainer.jsx
@@ -0,0 +1,27 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useOutlet, useLocation } from "react-router-dom";
+import { useState } from "react";
+
+function AnimatedOutlet() {
+	const o = useOutlet();
+	const [outlet] = useState(o);
+
+	return <>{outlet}</>;
+}
+
+export default function RootContainer() {
+	const location = useLocation();
+
+	return (
+		<AnimatePresence mode="wait">
+			<motion.div
+				key={location.key}
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				exit={{ opacity: 0 }}
+			>
+				<AnimatedOutlet />
+			</motion.div>
+		</AnimatePresence>
+	);
+}
