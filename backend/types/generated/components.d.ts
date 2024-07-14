baf86f2b2402ad34e1cc87bebc3b1f1684ed2f3c
Mike Newberry <hello@mikenew.xyz>
2024-07-14 13:40:39 +0100

    upgrade strapi to 5

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,34 @@
-import type { Schema, Attribute } from '@strapi/strapi';
+import type { Struct, Schema } from '@strapi/strapi';
 
-declare module '@strapi/types' {
-  export module Shared {}
+export interface HomeHeader extends Struct.ComponentSchema {
+  collectionName: 'components_home_headers';
+  info: {
+    displayName: 'Header';
+    description: '';
+  };
+  attributes: {
+    topText: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'hello there'>;
+    defaultTitleText: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<"i'm mike">;
+    subTitleText: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'Experienced British Web Developer'>;
+    resumeButtonText: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'resume'>;
+    projectsButtonText: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'projects'>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'home.header': HomeHeader;
+    }
+  }
 }
