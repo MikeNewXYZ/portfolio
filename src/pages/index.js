935209e09dca1064b02e6595ab967e154b749d9a
Mike Newberry <hello@mikenew.xyz>
2024-07-18 20:48:03 +0100

    add Home page data api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,27 @@
 import Head from "next/head";
+import HomeBackground from "@/backgrounds/HomeBackground/HomeBackground";
 
 // TODO: ADD Open graph metadata
 // TODO: ADD Animated background
 // TODO: ADD Floating header with navigation
 // TODO: ADD Contact link
 
-export default function HomePage() {
+export async function getStaticProps() {
+	try {
+		const response = await fetch("https://portfolio-cms.mikenew.xyz/items/home_page");
+		const responseObject = await response.json();
+
+		if (!responseObject) return { notFound: true };
+
+		return { props: { pageData: responseObject.data } };
+	} catch (error) {
+		return { notFound: true };
+	}
+}
+
+export default function HomePage({ pageData }) {
+	console.log(pageData);
+
 	return (
 		<>
 			<Head>
@@ -16,6 +32,8 @@ export default function HomePage() {
 			</Head>
 
 			<main> hello world </main>
+
+			<HomeBackground />
 		</>
 	);
 }
