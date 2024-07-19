a9408365e53a535b07a408066374031635063181
Mike Newberry <hello@mikenew.xyz>
2024-07-19 11:33:40 +0100

    actual fix for page transition bug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import "@/styles/globals.css";
 import { useRouter } from "next/router";
 import { AnimatePresence } from "framer-motion";
+import useFoucFix from "@/hooks/useFoucFix/useFoucFix";
 import RootLayout from "@/layouts/RootLayout/RootLayout";
 
 export default function App({ Component, pageProps }) {
+	useFoucFix();
 	const router = useRouter();
 
 	return (
