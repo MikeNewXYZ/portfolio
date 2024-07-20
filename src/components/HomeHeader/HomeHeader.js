ae595e8e3f5467f70e1ecebc9fc74b0e1bc092ad
Mike Newberry <hello@mikenew.xyz>
2024-07-20 22:07:20 +0100

    fix headerTitle fading in and out on page load

--- src/components/HomeHeader/HomeHeader.js
+++ src/components/HomeHeader/HomeHeader.js
@@ -1,7 +1,8 @@
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import useFloatingCard from "@/hooks/useFloatingCard/useFloatingCard";
 import Button from "../Button/Button";
+import HeaderTitle from "./HeaderTItle/HeaderTitle";
 
 const headerAnimation = {
 	hide: {
@@ -18,11 +19,6 @@ const headerAnimation = {
 	},
 };
 
-const titleAnimation = {
-	hide: { opacity: 0 },
-	show: { opacity: 1 },
-};
-
 export default function HomeHeader({
 	topText = "",
 	title = "",
@@ -33,28 +29,13 @@ export default function HomeHeader({
 }) {
 	const { outerRef, innerRef } = useFloatingCard();
 
-	// Prevents title text from disapearing when title prop changes too quickly.
-	const titleKey = `${title}-${Math.random()}`;
-
 	return (
 		<motion.div ref={outerRef} variants={headerAnimation} initial="hide" animate="show" exit="hide">
 			<div ref={innerRef}>
 				<div className="text-center">
 					<h2 className="text-3xl font-black uppercase sm:text-4xl">{topText}</h2>
 
-					<AnimatePresence mode="wait" initial={false}>
-						<motion.h1
-							key={titleKey}
-							className="text-6xl font-black uppercase sm:text-8xl"
-							variants={titleAnimation}
-							initial="hide"
-							animate="show"
-							exit="hide"
-							transition={{ duration: 0.5 }}
-						>
-							{title}
-						</motion.h1>
-					</AnimatePresence>
+					<HeaderTitle title={title} />
 
 					<p className="mt-2 text-sm normal-case sm:text-base">{subText}</p>
 				</div>
