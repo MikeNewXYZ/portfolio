89c9edb19d5d5c6384a99e359295cb04f18e02cd
Mike Newberry <hello@mikenew.xyz>
2024-07-20 14:46:43 +0100

    remove resume page, add reverse proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	async rewrites() {
+		return [
+			{
+				source: "/assets/:id",
+				destination: "https://portfolio-cms.mikenew.xyz/assets/:id",
+			},
+		];
+	},
 };
 
 export default nextConfig;
