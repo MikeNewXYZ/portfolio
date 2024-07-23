b517bf51fe12eda3d2618d30844d216a6668538f
Mike Newberry <hello@mikenew.xyz>
2024-07-23 17:23:40 +0100

    add disable state to button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,18 @@
 import { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-const Button = forwardRef(function Button({ className, children, ...props }, ref) {
+const Button = forwardRef(function Button(
+	{ className, children, disabled = false, ...props },
+	ref,
+) {
 	return (
 		<button
 			{...props}
 			ref={ref}
 			className={twMerge(
-				"border-2 px-3 py-1.5 text-sm uppercase transition-transform duration-500 ease-in-out hover:-translate-y-1 active:scale-95",
+				"select-none border-2 px-3 py-1.5 text-sm uppercase transition-all duration-500 ease-in-out hover:-translate-y-1 active:scale-95",
 				className,
+				disabled && "pointer-events-none opacity-50",
 			)}
 		>
 			{children}
