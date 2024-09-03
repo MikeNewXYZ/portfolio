d37bc2a6d27810c9103059fee6bb15905c652da6
Mike Newberry <hello@mikenew.xyz>
2024-09-03 17:01:47 +0100

    add theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			primary: "#E7E0CD",
+			secondary: "#000000",
+		},
 		extend: {},
 	},
 	plugins: [],
