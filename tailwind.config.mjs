9fdac0b21ed1f67eb6c38e9ae7efbd85e1e4d852
Mike Newberry <hello@mikenew.xyz>
2024-09-04 00:12:17 +0100

    add button tw component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,5 +10,14 @@ export default {
 		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		function ({ addComponents }) {
+			addComponents({
+				".button": {
+					"@apply block select-none border-2 px-3 py-1.5 text-sm uppercase transition-all duration-500 ease-in-out hover:-translate-y-1 active:scale-95":
+						{},
+				},
+			});
+		},
+	],
 };
