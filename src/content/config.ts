85366ccb033424e021078e192419b9e9bc960de4
Mike Newberry <hello@mikenew.xyz>
2024-09-05 13:06:57 +0100

    add Image to projects schema, add project card

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,23 +2,25 @@ import { z, defineCollection } from "astro:content";
 
 const projectsCollection = defineCollection({
 	type: "data",
-	schema: z.object({
-		title: z.string(),
-		subtitle: z.string(),
-		technologies: z.array(z.string()),
-		viewLink: z
-			.object({
-				label: z.string(),
-				url: z.string().url(),
-			})
-			.optional(),
-		repoLink: z
-			.object({
-				label: z.string(),
-				url: z.string().url(),
-			})
-			.optional(),
-	}),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			subtitle: z.string(),
+			thumbnail: image(),
+			technologies: z.array(z.string()),
+			viewLink: z
+				.object({
+					label: z.string(),
+					url: z.string().url(),
+				})
+				.optional(),
+			repoLink: z
+				.object({
+					label: z.string(),
+					url: z.string().url(),
+				})
+				.optional(),
+		}),
 });
 
 export const collections = {
