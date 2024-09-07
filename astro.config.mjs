4c5727abda19af1ac6a76e075c16af9881c2c812
Mike Newberry <hello@mikenew.xyz>
2024-09-07 13:00:50 +0100

    add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), react(), markdoc(), keystatic()],
 	output: "hybrid",
+
+	adapter: node({
+		mode: "standalone",
+	}),
 });
