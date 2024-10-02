c6be47a40bc9802c4b3a6c92a7e5db8eef17f59e
Mike Newberry <hello@mikenew.xyz>
2024-10-02 23:42:30 +0100

    add alpine intersect plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://mikenew.xyz",
-	integrations: [tailwind(), alpinejs(), icon()],
+	integrations: [tailwind(), alpinejs({ entrypoint: "/src/alpinejs.entrypoint.ts" }), icon()],
 });
