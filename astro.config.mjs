5c02323fc5acbe66361b5ec999e6fbbc03975e5d
Mike Newberry <hello@mikenew.xyz>
2024-10-02 16:34:40 +0100

    add astro icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://mikenew.xyz",
-	integrations: [tailwind(), alpinejs()],
+	integrations: [tailwind(), alpinejs(), icon()],
 });
