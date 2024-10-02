20a1082f631555fd9f7eb584abdf0da065095d84
Mike Newberry <hello@mikenew.xyz>
2024-10-02 16:33:07 +0100

    add alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://mikenew.xyz",
-	integrations: [tailwind()],
+	integrations: [tailwind(), alpinejs()],
 });
