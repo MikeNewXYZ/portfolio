c74e87516f8db0a0305297284f65f44d36e86715
Mike Newberry <hello@mikenew.xyz>
2024-10-03 18:35:16 +0100

    change output to server with node

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import icon from "astro-icon";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://mikenew.xyz",
+	output: "server",
+	adapter: node({
+		mode: "standalone",
+	}),
 	integrations: [tailwind(), alpinejs({ entrypoint: "/src/alpinejs.entrypoint.ts" }), icon()],
 });
