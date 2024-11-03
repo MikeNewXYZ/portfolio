9a12bcdd9c8d4477d3cbd76b7e02cbf1eae23a4c
Mike Newberry <hello@mikenew.xyz>
2024-11-03 23:50:58 +0000

    add and sync directus data

---
+++ directus-sync.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+	directusUrl: process.env.DIRECTUS_URL,
+	directusEmail: process.env.DIRECTUS_EMAIL,
+	directusPassword: process.env.DIRECTUS_PASSWORD,
+	dumpPath: "./directus-config",
+};
