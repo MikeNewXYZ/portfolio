035ab726a86ca85527f17a1c3dd1c9a885f78b2e
Mike Newberry <hello@mikenew.xyz>
2024-12-20 15:11:42 +0000

    add import formater

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,15 +1,15 @@
 // storage-adapter-import-placeholder
 import { sqliteAdapter } from "@payloadcms/db-sqlite";
+import { nodemailerAdapter } from "@payloadcms/email-nodemailer";
 import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import path from "path";
 import { buildConfig } from "payload";
-import { fileURLToPath } from "url";
 import sharp from "sharp";
-import { nodemailerAdapter } from "@payloadcms/email-nodemailer";
+import { fileURLToPath } from "url";
 
-import { Users } from "./collections/Users";
 import { Media } from "./collections/Media";
+import { Users } from "./collections/Users";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
